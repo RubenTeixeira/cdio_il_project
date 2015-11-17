36243975d2ae43cfb1d7eebabbf41b8c567cf296
Marco Sousa <1140864@isep.ipp.pt>
2015-11-17 21:13:52 +0000

    CDDIEP-47 closes Actualização menu de opções

--- Drop/src/ui/main.java
+++ Drop/src/ui/main.java
@@ -16,44 +16,49 @@ import java.util.logging.Logger;
  * @author vascopinho
  */
 public class main {
-    
+
     static Scanner in = new Scanner(System.in);
-    
-    public static void main(String[] args) throws SQLException{
+
+    public static void main(String[] args) throws SQLException {
         utils.ReadAndWriteFile.readFromFile("settings.txt");
-        
+
         int op = 0;
-       
-        do{
+
+        do {
             menu();
             op = in.nextInt();
-            switch(op){
+            switch (op) {
                 case 1:
                     try {
                         new ConsultarOcupacaoEntregaUI();
                     } catch (RuntimeException e) {
-                            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, e.getMessage());
+                        Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, e.getMessage());
                     }
                     break;
-                    
+
                 case 2:
                     new ComprarServicoDPUI();
-                
+                    break;
+                    
                 case 3:
+                    new AbrirPrateleiraUI();
+                    break;
+                case 4:
                     exit(0);
                     break;
                 default:
                     System.out.println("Opção inválida");
             }
-        }while(op != 3);
-        
+        } while (op != 4);
+
     }
-    
-    public static void menu(){
-        String menu="---------MENU---------\n"
+
+    public static void menu() {
+        String menu = "---------MENU---------\n"
                 + "1. Gestão DropPoint\n"
                 + "2. Comprar serviço DropPoint\n"
-                + "3. Sair";
+                + "3. Abrir Prateleira\n"
+                + "4. Sair";
         System.out.println(menu);
     }
 }
