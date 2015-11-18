c392db1d42092895e6471737409233420701371b
Your Name <1140358@isep.ipp.pt>
2015-11-18 11:16:05 +0000

    Implementacao do Padrao MAPPER

---
+++ Drop/src/domain/RegistoCliente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistence.OracleDb;
+import persistence.SQLConnection;
+
+/**
+ *
+ * @author nuno
+ */
+public class RegistoCliente {
+
+    private SQLConnection conn;
+
+    private String insert = "Insert Into Cliente (email,telemovel,username,password) VALUES (?,?,?,?))";
+    private String update = "Update Cliente (email,telemovel,username,password) VALUES (?,?,?,?))";
+
+    public RegistoCliente() {
+        conn = OracleDb.getInstance();
+    }
+
+    public boolean registarCliente(Cliente novoCliente) {
+        PreparedStatement prepareStatement = conn.prepareStatement(insert);
+        try {
+            
+            prepareStatement.setString(1, novoCliente.getEmail());
+            prepareStatement.setInt(2, novoCliente.getTelemovel());
+            prepareStatement.setString(3, novoCliente.getUsername());
+            prepareStatement.setString(4, novoCliente.getPassword());
+            
+            return prepareStatement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistoCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+    }
+
+    public Cliente login(String username, String password) {
+        return null;
+    }
+
+}
