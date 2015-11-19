7b9ed5dd0d7bffb2e5288439f0f21845cef584d6
Your Name <1140358@isep.ipp.pt>
2015-11-19 19:37:37 +0000

    CDDIEP-37  REFS ISSUE - Terminado Controller registar Cliente

--- Drop/src/controller/RegistarClienteController.java
+++ Drop/src/controller/RegistarClienteController.java
@@ -4,19 +4,27 @@ import domain.Cliente;
 import domain.Gestao;
 import domain.Morada;
 import domain.RegistoCliente;
+import domain.RegistoMorada;
 
 public class RegistarClienteController {
 
-    private Gestao gestao;
+   
     private RegistoCliente registoCliente;
+    private RegistoMorada registoMorada; 
+    private Morada morada;
 
     public RegistarClienteController() {
-        this.gestao = new Gestao();
         this.registoCliente = new RegistoCliente();
+        this.registoMorada = new RegistoMorada(registoCliente.getConn());
     }
+    
+    public boolean novaMorada(String rua, String codigoPostal, String localidade){
+        morada = registoMorada.novaMorada(rua, codigoPostal, localidade);
+        return registoMorada.registarMorada(morada);
+    }
+    
 
-    public boolean novoCliente(int id, String nome, int NIF, String username, String password, Morada morada, String email, int telemovel) {
-
+    public boolean novoCliente(int id, String nome, int NIF, String username, String password, String email, int telemovel) {
         Cliente novoCliente = registoCliente.novoCliente(id, nome, NIF, username, password, morada, email, telemovel);
         registoCliente.registarCliente(novoCliente);
         return (novoCliente != null);
