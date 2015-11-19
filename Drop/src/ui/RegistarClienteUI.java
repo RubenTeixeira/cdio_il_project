0f6e4e50c8999ea9189203a697d262075cf2068e
Your Name <1140358@isep.ipp.pt>
2015-11-19 19:37:22 +0000

    CDDIEP-37  REFS ISSUE - Terminado UI registar Cliente

--- Drop/src/ui/RegistarClienteUI.java
+++ Drop/src/ui/RegistarClienteUI.java
@@ -1,8 +1,6 @@
-
 package ui;
 
 import controller.RegistarClienteController;
-import domain.Cliente;
 import domain.Morada;
 
 public class RegistarClienteUI {
@@ -17,25 +15,45 @@ public class RegistarClienteUI {
     private void run() {
 
         System.out.println("----Insira dados de contacto----\n");
-        System.out.println("Email\n");
+        
+        System.out.println("\n----Nome: ");
+        String nome = utils.ReadFromKeyboard.readString();
+        
+        System.out.println("\n----NIF: ");
+        int NIF = utils.ReadFromKeyboard.read();
+        
+        System.out.println("\n----Email: ");
         String email = utils.ReadFromKeyboard.readString();
-        System.out.println("Telemóvel\n");
+        
+        System.out.println("\n----Telemóvel: ");
         int telemovel = utils.ReadFromKeyboard.read();
-        System.out.println("Username\n");
+        
+        System.out.println("\n----Username: ");
         String username = utils.ReadFromKeyboard.readString();
-        System.out.println("Password\n");
+        
+        System.out.println("\n----Password: ");
         String password = utils.ReadFromKeyboard.readString();
-        System.out.println("--Insira morada--\n");
-        System.out.println("Rua:\n");
+        
+        System.out.println("----Insira Morada----\n");
+        
+        System.out.println("\n----Rua:");
         String rua = utils.ReadFromKeyboard.readString();
-        System.out.println("Código Postal:\n");
+        
+        System.out.println("\n----Código Postal:\n");
         String codigoPostal = utils.ReadFromKeyboard.readString();
-        System.out.println("Localidade:\n");
+        
+        System.out.println("\n----Localidade:\n");
         String localidade = utils.ReadFromKeyboard.readString();
 
-        Morada morada=new Morada(rua,codigoPostal,localidade);
+        controller.novaMorada(rua, codigoPostal, localidade);
         
-        //Cliente cliente=new Cliente(email,telemovel,username,password,morada);
-        
+        if (controller.novoCliente(7, nome, NIF, username, password, email, telemovel)) {
+            System.out.println("Registado com sucesso");
+        } else {
+            System.out.println("Algo de errado aconteceu. Tente mais tarde.");
+
+        }
+
     }
+
 }
